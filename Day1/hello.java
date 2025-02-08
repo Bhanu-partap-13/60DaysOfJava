dbf5b8a3f23cca22badb27803893014e39cc9d43
{Bhanu <partapbhanu516@gmail.com>
2025-02-08 11:54:05 +0530

    Day01

---
+++ Day1/hello.java
@@ -0,0 +1,27 @@
+// package Day1; - this is the package name use to organize the classes in a logical manner
+// import java.util.Scanner; //Import the Scanner class (We will Discuss in input.java)
+
+public class hello {
+    // Single Line comments in Java
+    /*
+     Multi Line comments in Java
+     */
+     /** Documentation comment */
+  /*   public interface Innermain {
+        public static void main(String[] args) {
+            // Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            // System.out.println("Enter username");
+            // String userName = myObj.nextLine();  // Read user input
+            // System.out.println("Username is: " + userName);  // Output user input
+            // myObj.close();
+     -----------------------------------------Here we have taken an example of interface and main method inside it. But we can't run this code because we can't have main method inside an interface. So, we have to remove the interface and run the code.-----------------------------------------
+        }   
+    } */
+   public static void main(String[] args){ //Main method class
+    System.out.println("Hello World"); //Prints Hello World
+   }
+    }
+// The extension we are using is- .java
+//Java is case - sensitive language.
+//The file name must match the class name. If the file name is hello.java, the class name must be hello.
+//Invalid class name - 1hello, helloworld, hello world
